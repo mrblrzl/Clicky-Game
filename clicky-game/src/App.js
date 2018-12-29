fa185799ee42c4696f94d81f4ea6c5bcecea56b6
mrblrzl <m.s.arzola@gmail.com>
2018-12-28 23:57:40 -0800

    functionality

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -6,13 +6,64 @@ import characters from "./characters.json";
 import Nav from "./components/Nav";
 import Wrapper from "./Wrapper";
 
+//Shuffle character cards 
+function randomFriends(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
+
 
 class App extends Component {
   // Setting this.state.friends to the friends json array
   state = {
-    characters
+    characters,
+    currentScore: 0,
+    topScore: 0,
+    rightWrong: "",
+    clicked: [],
   };
 
+  handleClick = id => {
+    if (this.state.clicked.indexOf(id) === -1) {
+      this.handleIncrement();
+      this.setState({ clicked: this.state.clicked.concat(id) });
+    } else {
+      this.handleReset();
+    }
+  };
+
+  handleIncrement = () => {
+    const newScore = this.state.currentScore + 1;
+    this.setState({
+      currentScore: newScore,
+      rightWrong: "Nice Streak!"
+    });
+    if (newScore >= this.state.topScore) {
+      this.setState({ topScore: newScore });
+    }
+    else if (newScore === 12) {
+      this.setState({ rightWrong: "You win!" });
+    }
+    this.handleShuffle();
+  };
+
+  handleReset = () => {
+    this.setState({
+      currentScore: 0,
+      topScore: this.state.topScore,
+      rightWrong: "Streak Over!",
+      clicked: []
+    });
+    this.handleShuffle();
+  };
+
+  handleShuffle = () => {
+    let shuffledFriends = randomFriends(characters);
+    this.setState({ characters: shuffledFriends });
+  };
   // removeFriend = id => {
   //   const friends = this.state.friends.filter(characters => characters.id !== id);
   //   this.setState({ friends });
@@ -34,10 +85,11 @@ class App extends Component {
         <Wrapper>
         {this.state.characters.map(characters => (
           <CharacterCard
-            id={characters.id}
-            key={characters.id}
-            name={characters.name}
-            image={characters.image}
+          key={characters.id}
+          handleClick={this.handleClick}
+          handleShuffle={this.handleShuffle}
+          id={characters.id}
+          image={characters.image}
           />
         ))}
         </Wrapper>
--- clicky-game/src/components/CharacterCard/index.js
+++ clicky-game/src/components/CharacterCard/index.js
@@ -1,16 +1,16 @@
 import React from "react";
 import "./style.css";
 
-function CharacterCard(props) {
-  return (
-    <div className="card">
-      <div className="img-container">
-        <img alt={props.name} src={props.image} />
-      </div>
-      {/* <span onClick={() => props.removeFriend(props.id)} className="remove">  
-      </span> */}
+const CharacterCard = props => (
+  <div 
+    className="card" 
+    value={props.id} 
+    onClick={() => props.handleClick(props.id)}
+  >
+    <div className="img-container">
+      <img alt={props.name} src={props.image} />
     </div>
-  );
-}
+  </div>
+);
 
 export default CharacterCard;

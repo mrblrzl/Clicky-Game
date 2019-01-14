cc2943703aa0cb1191c35f7598490a0e349754d8
mrblrzl <m.s.arzola@gmail.com>
2019-01-13 21:33:32 -0800

    Updates

---
+++ precache-manifest.53fc3a8d2ce3dd58cb2230609f59398e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f9ed21177874c072eef",
+    "url": "/Clicky-Game/static/js/runtime~main.2f9ed211.js"
+  },
+  {
+    "revision": "6990e63ad4422f566d1c",
+    "url": "/Clicky-Game/static/js/main.6990e63a.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/Clicky-Game/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "6990e63ad4422f566d1c",
+    "url": "/Clicky-Game/static/css/main.a3e86e1d.chunk.css"
+  },
+  {
+    "revision": "bfda3aa81798350aef95c70ef8815272",
+    "url": "/Clicky-Game/index.html"
+  }
+];
